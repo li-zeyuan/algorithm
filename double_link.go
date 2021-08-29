10c0a4f077daef03c9bee7ee242e387406e1fa28
Ahern <lzyutf8@163.com>
2021-08-29 18:09:13 +0800

    feat: fast == nil

--- double_link.go
+++ double_link.go
@@ -154,16 +154,13 @@ func hasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
 	}
-
 	slow, fast := head, head.Next
-	for slow != fast {
-		if slow == nil || fast.Next == nil {
+	for fast != slow {
+		if fast == nil || fast.Next == nil {
 			return false
 		}
-
 		slow = slow.Next
 		fast = fast.Next.Next
 	}
-
 	return true
 }
