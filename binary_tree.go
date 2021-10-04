8c701efb087993cf79565dd5e5aac9a2d86aeb92
Ahern <lzyutf8@163.com>
2021-10-04 15:08:15 +0800

    feat: 反转二叉树

--- binary_tree.go
+++ binary_tree.go
@@ -166,3 +166,19 @@ func (b *BinaryTree) Reverse() {
 参考
 - https://learnku.com/articles/48638
 */
+
+/*
+反转二叉树
+https://leetcode-cn.com/problems/invert-binary-tree/
+*/
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+
+	invertTree(root.Right)
+	invertTree(root.Left)
+	return root
+}
